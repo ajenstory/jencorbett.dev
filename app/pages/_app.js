b22b5bb97ed80c7f1969e445c77af8250d6151a5
Jen Corbett <104407983+crashtestdolly@users.noreply.github.com>
2023-03-18 19:00:16 +1300

    Setup (#1)

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,43 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import "../styles/globals.css";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <select
+      value={theme}
+      onChange={(e) => setTheme(e.target.value)}
+    >
+      <option value='system'>System</option>
+      <option value='dark'>Dark</option>
+      <option value='light'>Light</option>
+    </select>
+  );
+};
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <ThemeProvider>
+        <ThemeSwitch />
+        <Component {...pageProps} />{" "}
+      </ThemeProvider>{" "}
+    </>
+  );
+};
+
+export default App;
