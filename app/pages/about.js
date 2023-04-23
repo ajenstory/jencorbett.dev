2b5a0b30ef93c0ba342378d2e3bb5403f664ac4e
jen <crashtestdollynz@gmail.com>
2023-04-23 14:08:45 +1200

    Set up about page grid

--- app/pages/about.js
+++ app/pages/about.js
@@ -1,16 +1,44 @@
 import utilStyles from "../styles/utils.module.css";
-
+import styles from "../styles/home.module.css";
+import Card from "../components/card";
+import Image from "next/image";
+import css3 from "../public/assets/css3.svg";
 import Header from "../components/header";
 import Link from "next/link";
 const AboutPage = () => {
   return (
     <>
-      <Header />
-      <h1>About me</h1>
-      <p>
-        <code>{`"name": "crashTestDolly"`}</code>
-      </p>
-      <Link className={utilStyles.center} href="/">
+      <Header /> <h1>About me</h1>
+      <section>
+        <div className={`${styles.card}${styles.grid} ${styles.center}`}>
+          {" "}
+          <Card>
+            <Image
+              width="60"
+              height="60"
+              src={css3}
+            ></Image>
+          </Card>
+          <Card>
+            <Image
+              width="60"
+              height="60"
+              src={css3}
+            ></Image>
+          </Card>
+          <Card>
+            <Image
+              width="60"
+              height="60"
+              src={css3}
+            ></Image>
+          </Card>
+        </div>
+      </section>
+      <Link
+        className={utilStyles.center}
+        href="/"
+      >
         ← Back to home{" "}
       </Link>
     </>
