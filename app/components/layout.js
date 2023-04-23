8db9f39e4dcd27f13145e51bbf1de4704119acf3
jen <crashtestdollynz@gmail.com>
2023-04-23 15:50:27 +1200

    Start setting up visibility prop

--- app/components/layout.js
+++ app/components/layout.js
@@ -4,14 +4,26 @@ import utilStyles from "../styles/utils.module.css";
 import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
-const Layout = ({ children, main }) => {
+const Layout = ({ children, visible }) => {
   return (
     <div>
-      <main
-        className={`${inter.className} ${utilStyles} ${styles} ${styles.main}`}
-      >
-        {children}
-      </main>
+      <div>
+        {`${children} && ${visible}` ? (
+          <>
+            <div>
+              <main
+                className={`${inter.className} ${utilStyles} ${styles} ${styles.main}`}
+              >
+                {children}
+              </main>
+            </div>
+          </>
+        ) : (
+          <>
+            <main className={` ${utilStyles.invisible}`}>{children}</main>
+          </>
+        )}
+      </div>
     </div>
   );
 };
