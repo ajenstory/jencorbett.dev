63b533cce113215faf7a7d1a4ef05e89741ea47e
jen <crashtestdollynz@gmail.com>
2023-04-26 17:27:18 +1200

    Adjust layout

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,35 +1,41 @@
 import Link from "next/link";
 import { Inter } from "next/font/google";
-
 import utilStyles from "../styles/utils.module.css";
 import styles from "../styles/Home.module.css";
 import Card from "../components/card";
+
 import Header from "../components/header";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <Header splash />
-      <div className={styles}>
-        <h1 className={`${utilStyles.headingXL}`}>Hello</h1>
-        <div>
-          <div className={styles.description.p}>
-            {" "}
-            <p className={inter.className}>
-              Hi! I'm Jen. I'm an aspiring software developer with a background
-              in digital and social media. Thanks for stopping by!
-            </p>
-            <p className={inter.className}>
-              I'm currently working on this portfolio site, so there's not much
-              to see here yet.
-            </p>{" "}
-            <p className={inter.className}>
-              In the meantime, please get in touch with me on Linkedin or by
-              emailing `hello [at] jencorbett.dev`.
-            </p>{" "}
-          </div>
-        </div>{" "}
+      <Header home />
+
+      <div className={`${inter.className}  ${styles.center} ${styles.grid}`}>
+        {" "}
+        <Card>
+          {" "}
+          <h1 className={`${utilStyles.headingLg}`}>Hello</h1>{" "}
+          <p className={` ${styles}`}>
+            I'm Jen...chief storyteller, early adopter, "late" developer and
+            veteran of the social media era.
+          </p>{" "}
+          <p>
+            I'm currently working on this site. So there's not much to see here
+            yet. In time, I'll have be growing a little digital garden, as a
+            sort of digital garden and porfolio, with some writing to share and
+            project stuff to show off too.
+          </p>
+          <p>
+            In the meantime, please don't hesitate to reach out on Linkedin or
+            by emailing me on{" "}
+            <span className={` ${utilStyles.bold} `}>
+              hello[at]jencorbett.dev
+            </span>
+            .
+          </p>
+        </Card>{" "}
       </div>
       <Card>
         <Link href="https://linkedin.com/in/jencorbett">
@@ -45,40 +51,20 @@ export default function Home() {
           </h2>
         </Link>
       </Card>
-      <div className={`${styles.hide}`}>
-        <Card>
-          <Link href="/projects">
-            <h2 className={`${styles.hide}`}>
-              Projects<span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>see more</p>
-          </Link>
-        </Card>
-        <Card>
-          <Link href="/posts">
-            <h2 className={`${styles.hide}`}>
-              Posts <span>-&gt;</span>
-            </h2>
-            <p className={`${styles.hide}`}>see more</p>
-          </Link>
-        </Card>
-        <Card>
-          <Link href="/about">
-            <h2 className={`${styles.hide}`}>
-              About <span>-&gt;</span>
-            </h2>
-            <p className={`${styles.hide}`}>see more</p>
-          </Link>
-        </Card>
-        <Card>
-          <Link href="/contact">
-            <h2 className={`${styles.hide}`}>
-              Contact <span>-&gt;</span>
-            </h2>
-            <p className={`${styles.hide}`}>see more</p>
-          </Link>
-        </Card>
-      </div>
+      <Card>
+        <Link href="https://mastodon.nz/@jen">
+          <h2 className={``}>
+            Mastodon <span>-&gt;</span>
+          </h2>
+        </Link>
+      </Card>
+      <Card>
+        <Link href="https://twitter.com/@ajenstory">
+          <h2 className={``}>
+            Twitter <span>-&gt;</span>
+          </h2>
+        </Link>
+      </Card>
     </>
   );
 }
