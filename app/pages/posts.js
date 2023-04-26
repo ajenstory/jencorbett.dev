5f9692be280e46c8d1c588aa2b246f7618e8115e
jen <crashtestdollynz@gmail.com>
2023-04-26 12:14:26 +1200

    Update posts page to use contentlayer

--- app/pages/posts.js
+++ app/pages/posts.js
@@ -1,53 +1,59 @@
 import utilStyles from "../styles/utils.module.css";
 import Date from "../components/date";
-// import { getSortedPostsData } from "../lib/posts";
+import { compareDesc } from "date-fns";
 import Link from "next/link";
 import Header from "../components/header";
-import { MDXProvider } from "@mdx-js/react";
-export default function PostsPage({ posts }) {
-  return (
-    <>
-      <MDXProvider>
-        <Header />{" "}
-        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-          <h1>Posts</h1>
-          <ul className={utilStyles.list}>
-            {posts.map(({ id, date, title }) => (
-              <li
-                className={utilStyles.listItem}
-                key={id}
-              >
-                <Link href={`/projects/${id}`}>{title}</Link>
-                <br />
-                <small className={utilStyles.lightText}>
-                  <Date dateString={JSON.parse(JSON.stringify(date))} />
-                </small>
-              </li>
-            ))}
-          </ul>{" "}
-        </section>
-      </MDXProvider>{" "}
-      <Link
-        className={utilStyles.center}
-        href="/"
-      >
-        ← Back to home{" "}
-      </Link>{" "}
-    </>
-  );
-}
-
+import Layout from "components/layout";
+import { allPosts } from "contentlayer/generated";
+console.log(allPosts);
 export async function getStaticProps() {
   const posts = allPosts.sort((a, b) => {
     return compareDesc(new Date(a.date), new Date(b.date));
   });
   return { props: { posts } };
 }
-// export async function getStaticProps() {
-//   const posts = getSortedPostsData();
-//   return {
-//     props: {
-//       posts,
-//     },
-//   };
-// }
+function PostCard(post) {
+  return (
+    <article className="">
+      <p>
+        {" "}
+        <Link href={post.url}>{post.title}</Link>
+      </p>
+    </article>
+  );
+}
+
+export default function PostPage({ posts }) {
+  return (
+    <>
+      <Header />
+      <Layout>
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <div className="">
+            <ul className={utilStyles.list}>
+              {posts.map((post, idx) => (
+                <li
+                  key={idx}
+                  className={utilStyles.listItem}
+                >
+                  <PostCard {...post}>{post.title}</PostCard>
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={JSON.parse(JSON.stringify(post.date))} />{" "}
+                  </small>{" "}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <small className={utilStyles.lightText}>
+            <Link
+              className={utilStyles.center}
+              href="/"
+            >
+              ← Back to home{" "}
+            </Link>{" "}
+          </small>
+        </section>
+      </Layout>
+    </>
+  );
+}
