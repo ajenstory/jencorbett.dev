36b6c83f895224acd5dc7b5387618573eafc18ad
jen <crashtestdollynz@gmail.com>
2023-04-26 17:50:53 +1200

    Update  next/image config

--- app/next.config.js
+++ app/next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
   // Configure pageExtensions to include md and mdx
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx", "mjs"],
   // Other Next.js config below
+  images: {
+    protocol: "https",
+    pathname: "/public/**",
+    // hostname: "**.jencorbett.dev",
+  },
   reactStrictMode: true,
 };
 
