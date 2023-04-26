e21434d05ef44bea9ca08ad8e016d82c8dbfe873
jen <crashtestdollynz@gmail.com>
2023-04-27 11:20:33 +1200

    Edit blurb for clarity

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,25 +8,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Header home />
       <div className={` ${styles.center} ${styles.grid}`}>
-        {" "}
         <Card>
-          {" "}
-          <h2 className={`${inter.className} `}>
-            From Hashtags to HTTP...
-          </h2>{" "}
-          <p className={` ${styles}`}>Hello, I'm Jen!</p>{" "}
+          <h2 className={`${inter.className} `}>Hello, I'm Jen!</h2>
+          <p> I'm a developer and digital strategist based in Ruakākā, Aotearoa New Zealand.</p>
           <p>
             I'm currently working on this site. So there's not much to see here
-            yet. In time, I'll have a lovely digital garden growing, with some
-            writing to share and projects to show off too!
-          </p>
-          <p>
-            In the meantime, please don't hesitate to reach out on Linkedin or
-            by emailing me on{" "}
-            <span className={` ${utilStyles.bold} `}>
+            yet! Please bookmark for now and check back soon. In the meantime,
+            feel free to reach out on LinkedIn or get in touch directly on{" "}
+            <span className={`${utilStyles.bold} `}>
               hello[at]jencorbett.dev
             </span>
             .
@@ -64,6 +56,6 @@ export default function Home() {
           </Link>
         </Card>
       </div>
-    </>
+    </div>
   );
 }
