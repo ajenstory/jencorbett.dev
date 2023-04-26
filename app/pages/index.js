a94d9e1d16092fba991cc336d24fe3f33cb9a905
jen <crashtestdollynz@gmail.com>
2023-04-26 18:50:40 +1200

    Add github link

--- app/pages/index.js
+++ app/pages/index.js
@@ -17,14 +17,11 @@ export default function Home() {
           <h2 className={`${inter.className} `}>
             From Hashtags to HTTP...
           </h2>{" "}
-          <p className={` ${styles}`}>
-            Hello, I'm Jen! Early adopter, "late" developer and veteran of the
-            social media era.
-          </p>{" "}
+          <p className={` ${styles}`}>Hello, I'm Jen!</p>{" "}
           <p>
             I'm currently working on this site. So there's not much to see here
-            yet. In time, I'll have a lovely digital garden growing here. A
-            porfolio, with some writing to share and projects to show off too.
+            yet. In time, I'll have a lovely digital garden growing, with some
+            writing to share and projects to show off too!
           </p>
           <p>
             In the meantime, please don't hesitate to reach out on Linkedin or
@@ -44,10 +41,11 @@ export default function Home() {
             </h2>
           </Link>
         </Card>
+
         <Card>
-          <Link href="mailto:hello@jencorbett.dev">
+          <Link href="https://github.com/ajenstory">
             <h2 className={``}>
-              Email <span>-&gt;</span>
+              Github <span>-&gt;</span>
             </h2>
           </Link>
         </Card>
