749f795d8574fe674f7ee498ed3be02d51580e6d
jen <crashtestdollynz@gmail.com>
2023-04-28 17:11:32 +1200

    Add refs

--- app/pages/api/spotify/player.js
+++ app/pages/api/spotify/player.js
@@ -2,8 +2,6 @@ import { getNowPlaying } from "../getNowPlaying";
 
 export const player = async () => {
   const response = await getNowPlaying();
-  console.log(response);
-
   const { item } = await response.json();
 
   const track = {
@@ -24,6 +22,15 @@ export default async function (req, res) {
     "Cache-Control",
     "public, s-maxage=86400, stale-while-revalidate=43200"
   );
-  console.log(response);
+
   return res.status(200).json(response);
+}
+
+{
+  /*
+
+Spotify player inspired by: [How to show Now Playing in Spotify with Next.js - Theodorus Clarence](https://theodorusclarence.com/blog/spotify-now-playing) & [How to show Now Playing in Spotify with Next.js - Theodorus Clarence](https://theodorusclarence.com/blog/spotify-now
+and also [How to use Spotify API with Next.js - Jatin Sharma](https://dev.to/j471n/how-to-use-spotify-api-with-nextjs-50o5)
+with a little help from [A Next.js complete beginner tutorial using the Spotify API - Salma Alam-Naylor](https://whitep4nth3r.com/blog/next-js-beginner-tutorial-using-spotify-api/)
+ */
 }
