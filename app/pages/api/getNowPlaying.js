d6399ca495001cab16c3993ad9a9215f87bfc5d6
jen <crashtestdollynz@gmail.com>
2023-04-28 17:02:42 +1200

    Create api route for spotify

---
+++ app/pages/api/getNowPlaying.js
@@ -0,0 +1,28 @@
+import queryString from "query-string";
+
+const getAccessToken = async () => {
+  const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${Buffer.from(
+        `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`
+      ).toString("base64")}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: queryString.stringify({
+      grant_type: "refresh_token",
+      refresh_token,
+    }),
+  });
+  return response.json();
+};
+
+export const getNowPlaying = async () => {
+  const { access_token } = await getAccessToken();
+  return fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+};
