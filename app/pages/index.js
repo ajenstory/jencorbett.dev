7a0e6a8ab7af8cfb494af9cc8c2d58d4dcfa691c
jen <crashtestdollynz@gmail.com>
2023-04-28 17:49:00 +1200

    Use swr to render api data on page

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,19 +1,25 @@
 import Link from "next/link";
 import HeadComponent from "@components/head";
-import Card from "../components/card";
+import div from "../components/card";
 import Header from "../components/header";
 import utilStyles from "@styles/utils.module.css";
 import styles from "@styles/Home.module.css";
+import Image from "next/image";
+
+import { SiSpotify } from "react-icons/si";
 import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
+import useSWR from "swr";
 export default function Home() {
   return (
     <div>
       <HeadComponent />
+
       <Header home />
+
       <div className={` ${styles.center} ${styles.grid}`}>
-        <Card>
+        <div>
           <h2 className={`${inter.className} `}>Hello, I'm Jen!</h2>
           <p>
             {" "}
@@ -29,39 +35,92 @@ export default function Home() {
             </span>
             .
           </p>
-        </Card>{" "}
+        </div>{" "}
       </div>
       <div className={`${styles.div} ${styles.grid}`}>
-        <Card>
+        <div>
           <Link href="https://linkedin.com/in/jencorbett">
             <h2 className={``}>
               Linkedin <span>-&gt;</span>
             </h2>
           </Link>
-        </Card>
+        </div>
 
-        <Card>
+        <div>
           <Link href="https://github.com/ajenstory">
             <h2 className={``}>
               Github <span>-&gt;</span>
             </h2>
           </Link>
-        </Card>
-        <Card>
+        </div>
+        <div>
           <Link href="https://mastodon.nz/@jen">
             <h2 className={``}>
               Mastodon <span>-&gt;</span>
             </h2>
           </Link>
-        </Card>
-        <Card>
+        </div>
+        <div>
           <Link href="https://twitter.com/@ajenstory">
             <h2 className={`${styles.card.h2}`}>
               Twitter <span>-&gt;</span>
             </h2>
           </Link>
-        </Card>
+        </div>
+      </div>
+      <div>
+        {" "}
+        <small className={` ${styles.center} ${styles.grid}`}>
+          <NowPlaying />
+        </small>
       </div>
     </div>
   );
 }
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const NowPlaying = () => {
+  const { data, error } = useSWR("/api/spotify/player", fetcher);
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <div className={` ${styles.center}`}>
+      <SiSpotify
+        size={64}
+        color={"#1ED760"}
+      />
+      <div>
+        <Image
+          src={data.albumImageUrl}
+          height={64}
+          width={64}
+          alt="Spotify album cover art"
+        ></Image>
+      </div>{" "}
+      <div>
+        <p>
+          {console.log(data)}
+          {data.name}
+        </p>
+      </div>
+      <div>
+        <p>
+          {console.log(data)}
+          {data.artist}
+        </p>
+      </div>
+      <div>
+        <p>
+          {console.log(data)}
+          {data.artist}
+        </p>
+      </div>
+      <div>
+        {" "}
+        <p>{console.log(data)}</p>{" "}
+      </div>
+    </div>
+  );
+};
