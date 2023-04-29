a23c885c39bf7eb7c28726c8f8fa9d9db177109d
jen <crashtestdollynz@gmail.com>
2023-04-29 19:58:12 +1200

    Update content

--- app/pages/index.js
+++ app/pages/index.js
@@ -9,16 +9,11 @@ export default function Home() {
         <Header home />
         <div>
           <Card>
-            {" "}
-            <h2>Hello, I'm Jen!</h2>
             <p className={``}>
               I'm a developer and web designer based in Ruakākā, Aotearoa New
-              Zealand. Thanks for checking out my personal porfolio site. As you
-              can see, this is a work in progress.
-            </p>
-            <p>
-              Feel free to reach out on LinkedIn or email me on
-              hello[at]jencorbett.dev to check my availability.
+              Zealand. To say hi or check my availability, feel free to reach
+              out to me on your social channel of choice or email me on
+              hello[at]jencorbett.dev.
             </p>
             <NowPlaying />
           </Card>
