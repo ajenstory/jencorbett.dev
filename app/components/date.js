ad71553d3be84b201210f401f1a00ebc1a3d458c
jen <crashtestdollynz@gmail.com>
2023-04-30 17:44:37 +1200

    update time on date util

--- app/components/date.js
+++ app/components/date.js
@@ -1,6 +1,13 @@
 import { parseISO, format } from "date-fns";
 
-export default function Date({ dateString }) {
+export function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "d,LLLL , yyyy p")}</time>;
+  return <time dateTime={dateString}>{format(date, "LLLL d yyyy")}</time>;
 }
+
+export function Time({ dateString }) {
+  const time = parseISO(dateString);
+  return <time dateTime={dateString}>{format(time, "p")}</time>;
+}
+
+export default { Date, Time };
