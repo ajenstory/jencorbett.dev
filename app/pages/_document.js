1d68f0dc8b08a90542f71ee61a6c9d222b0dd861
jen <crashtestdollynz@gmail.com>
2023-04-30 12:41:34 +1200

    Run prettier

--- app/pages/_document.js
+++ app/pages/_document.js
@@ -1,14 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-const title = "Jen Corbett Web Development, Design & Digital Strategy";
-
 export default function Document() {
   return (
     <Html lang="en">
-      {" "}
       <Head />
       <body>
-        {" "}
         <Main />
         <NextScript />
       </body>
