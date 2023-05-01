388f8344f188ca871b052d9bf8c3a9ffbe2aafbb
jen <crashtestdollynz@gmail.com>
2023-05-01 17:51:05 +1200

    Set up current date / time utils

--- app/components/date.js
+++ app/components/date.js
@@ -2,12 +2,28 @@ import { parseISO, format } from "date-fns";
 
 export function Date({ dateString }) {
   const date = parseISO(dateString);
+  console.log(date);
   return <time dateTime={dateString}>{format(date, "LLLL d yyyy")}</time>;
 }
 
 export function Time({ dateString }) {
   const time = parseISO(dateString);
-  return <time dateTime={dateString}>{format(time, "p")}</time>;
+  console.log(time);
+  return <time dateTime={dateString}>{format(time, "hh:mm:ss")}</time>;
 }
 
-export default { Date, Time };
+export function CurrentDate({ dateString }) {
+  const currentDate = new Date();
+  console.log(currentDate);
+  return (
+    <time dateTime={dateString}>{format(currentDate, "LLLL d yyyy")}</time>
+  );
+}
+
+export function CurrentTime({ dateString }) {
+  const currentTime = new Date();
+  console.log(currentTime);
+  return <time dateTime={dateString}>{format(currentTime, "hh:mm:ss")}</time>;
+}
+
+export default { Date, Time, CurrentDate, CurrentTime };
