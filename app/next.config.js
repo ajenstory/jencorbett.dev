63b1c6e099b3a538c36c74835a507d39d7a20d1e
jen <crashtestdollynz@gmail.com>
2023-05-02 14:31:01 +1200

    Remove custom config for  now

--- app/next.config.js
+++ app/next.config.js
@@ -18,33 +18,7 @@ const nextConfig = {
     // get latest build hash
     return "latest-build-hash";
   },
-  compiler: {
-    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
-    styledComponents: {
-      // Enabled by default in development, disabled in production to reduce file size,
-      // setting this will override the default for all environments.
-      displayName: false,
-      // Enabled by default.
-      ssr: false,
-      // Enabled by default.
-      fileName: false,
-      // Empty by default.
-      topLevelImportPaths,
-      // Defaults to ["index"].
-      meaninglessFileNames: ["index", "styles"],
-      // Enabled by default.
-      cssProp,
-      // Empty by default.
-      namespace,
-      // Not supported yet.
-      minify,
-      // Not supported yet.
-      transpileTemplateLiterals,
-      // Not supported yet.
-      pure,
-    },
-    reactStrictMode: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = withContentlayer(nextConfig);
