9735a67cde123886b3bb600234783a4ef758114b
jen <crashtestdollynz@gmail.com>
2023-05-02 12:57:29 +1200

    Add next compiler for styled components

--- app/next.config.js
+++ app/next.config.js
@@ -15,10 +15,36 @@ const nextConfig = {
     ],
   },
   generateBuildId: async () => {
-   // get latest build hash
+    // get latest build hash
     return "latest-build-hash";
   },
-  reactStrictMode: true,
+  compiler: {
+    // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
+    styledComponents: {
+      // Enabled by default in development, disabled in production to reduce file size,
+      // setting this will override the default for all environments.
+      displayName,
+      // Enabled by default.
+      ssr,
+      // Enabled by default.
+      fileName,
+      // Empty by default.
+      topLevelImportPaths,
+      // Defaults to ["index"].
+      meaninglessFileNames,
+      // Enabled by default.
+      cssProp,
+      // Empty by default.
+      namespace,
+      // Not supported yet.
+      minify,
+      // Not supported yet.
+      transpileTemplateLiterals,
+      // Not supported yet.
+      pure,
+    },
+    reactStrictMode: true,
+  },
 };
 
 module.exports = withContentlayer(nextConfig);
