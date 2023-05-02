0095ddaaf45288e190c445be39fdf7ddae9b052a
jen <crashtestdollynz@gmail.com>
2023-05-02 13:57:24 +1200

    add link style to footer

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,9 +1,21 @@
 import footerStyles from "./footer.module.css";
+import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className={`${footerStyles.container}   ${footerStyles.border}`}>
-      <footer>© Jen Corbett 2023</footer>{" "}
+    <div className={`${footerStyles.container}  ${footerStyles.border}`}>
+      <footer>
+        <p>
+          {" "}
+          © Jen Corbett 2023 &#183;{" "}
+          <Link
+            className={footerStyles.footerlink}
+            href="https://github.com/ajenstory/jencorbett.dev"
+          >
+            visit this project on github
+          </Link>
+        </p>
+      </footer>{" "}
     </div>
   );
 };
