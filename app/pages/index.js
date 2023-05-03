7f02e78b1a3c9b8b9d3e60fdda22e051f6f08898
jen <crashtestdollynz@gmail.com>
2023-05-03 17:46:11 +1200

    Add icons to index page content

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,11 +1,12 @@
+import IconWrapper from "@components/icon";
 import TextBlock from "@components/text";
 import indexStyles from "@styles/index.module.css";
-
+import { FaPaw } from "react-icons/fa";
 const description = {
   intro:
-    "Hello! I'm a developer based in Ruakākā, Aotearoa - New Zealand. As you can see, this is a work in progress.",
-  cta: "To say hi or check my availability, please feel free to email me on hello[at]jencorbett.dev",
-  social: "You can also find me on LinkedIn, twitter, or GitHub etc.",
+    "   Hello! I'm a developer based in Ruakākā, Aotearoa - New Zealand. As you can see, this is a work in progress.",
+  cta: "   To say hi or check my availability, please feel free to email me on hello[at]jencorbett.dev",
+  social: "   You can also find me on LinkedIn, twitter, or GitHub etc.",
 };
 
 export default function Home() {
@@ -13,9 +14,38 @@ export default function Home() {
     <>
       <div className={`${indexStyles.container} `}>
         <div className={`${indexStyles.grid} `}>
-          <TextBlock>{description.intro}</TextBlock>
-          <TextBlock>{description.cta}</TextBlock>
-          <TextBlock>{description.social}</TextBlock>
+          <div>
+            <IconWrapper
+              alt="icon"
+              href=""
+            >
+              {" "}
+              <FaPaw size={22} />
+            </IconWrapper>
+            <TextBlock>{description.intro}</TextBlock>
+          </div>
+          <div>
+            {" "}
+            <IconWrapper
+              alt="icon"
+              href=""
+            >
+              {" "}
+              <FaPaw size={22} />
+            </IconWrapper>
+            <TextBlock>{description.cta}</TextBlock>
+          </div>
+          <div>
+            {" "}
+            <IconWrapper
+              alt="icon"
+              href=""
+            >
+              {" "}
+              <FaPaw size={22} />
+            </IconWrapper>
+            <TextBlock> {description.social}</TextBlock>
+          </div>
         </div>
       </div>
     </>
