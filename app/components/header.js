5aafbc448000916e6e73a044287c7595475de9ed
jen <crashtestdollynz@gmail.com>
2023-05-04 18:04:57 +1200

    Adjust header padding

--- app/components/header.js
+++ app/components/header.js
@@ -13,13 +13,15 @@ const MainLayout = ({ home }) => {
           <>
             <div className={` ${headerStyles.header} `}>
               <div className={`${headerStyles.borderCircle}  `}>
-                <Image
-                  priority
-                  src={profilePic}
-                  height={108}
-                  width={108}
-                  alt="Profile picture of Jen deep in thought"
-                />{" "}
+                <Link href="https://github.com/ajenstory/jencorbett.dev">
+                  <Image
+                    priority
+                    src={profilePic}
+                    height={108}
+                    width={108}
+                    alt="Profile picture of Jen deep in thought"
+                  />{" "}
+                </Link>
               </div>
               <div className={headerStyles.title}>
                 <h1>{name}</h1>
