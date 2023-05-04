73355b5955e62362402f8a36b8b9697034188f63
jen <crashtestdollynz@gmail.com>
2023-05-05 11:12:43 +1200

    Tidy up date helper, create PlayedAt and shift back into components.

---
+++ app/components/date.js
@@ -0,0 +1,23 @@
+import { parseISO, format } from "date-fns";
+
+export const Date = ({ dateString }) => {
+  const date = parseISO(dateString);
+  console.log(date);
+  return (
+    <time dateTime={dateString}>
+      {format(date, "'on' EEE LLLL, dd, yyyy 'at' p")}
+    </time>
+  );
+};
+
+export const PlayedAt = ({ data }) => {
+  const played_at = JSON.parse(JSON.stringify(data));
+
+  return (
+    <>
+      <Date dateString={JSON.parse(JSON.stringify(played_at))} />{" "}
+    </>
+  );
+};
+
+export default { Date, PlayedAt };
