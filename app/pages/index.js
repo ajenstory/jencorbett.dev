9d15d0f16c3ab5f9bd39271f9c820b3ebf537d17
Jen Corbett <104407983+ajenstory@users.noreply.github.com>
2023-05-04 21:42:03 +1200

    Fix typo

--- app/pages/index.js
+++ app/pages/index.js
@@ -8,7 +8,7 @@ const description = {
   intro:
     "  I'm a developer based in Ruakākā, Aotearoa - New Zealand. As you can see, this is a work in progress.",
   cta: "   To say hi or check my availability, please feel free to email me on hello[at]jencorbett.dev",
-  social: "   You can also find me on LinkedIn, twitter, or GitHub etc.",
+  social: "   You can also find me on LinkedIn, Twitter, Mastodon or GitHub.",
 };
 
 export default function Home() {
