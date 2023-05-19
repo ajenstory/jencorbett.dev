521f7ebba63036adfdc25e7caee903aa876d06e6
jen <crashtestdollynz@gmail.com>
2023-05-19 18:54:07 +1200

    Update layout styles

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,36 +1,33 @@
 import indexStyles from "@styles/index.module.css";
-import Footer from "@components/footer";
+import Header from "@components/header.js";
+
 const description = {
-  intro: "I'm a developer based in Ruakākā, Aotearoa - New Zealand.",
-  about:
-    "I've recently graduated from my studies in Web Development and Design, transitioning from a background of more than 10 years in social media and digital marketing.",
+  title: "Hey, I'm Jen Corbett",
+  intro: "I'm currently working on my portfolio, please check back soon!",
+  cta: "I'm a web development graduate seeking opportunities to work in front-end development.",
   details:
-    "I'm currently working on a few projects, including this site while I seek internship and opportunities to gain technical experience.",
-  cta: " I'm also available for freelance work. To say hi or check my availability, please feel free to email me on hello[at]jencorbett.dev",
-  social: "You can also find me on LinkedIn, Twitter, Mastodon or GitHub.",
+    "To say hi or check my availability, please feel free to email me at hello[at]jencorbett.dev",
+  social:
+    "You can also connect with me on LinkedIn, Twitter, Mastodon, or GitHub.",
 };
 
 const Home = () => {
   return (
-    <>
-      {" "}
-      <section home>
-        <div className={indexStyles.indexContainer}>
-          <div className={indexStyles.wrapper}>
-            <p className={indexStyles.item}>{description.intro}</p>
-
-            <p className={indexStyles.item}>{description.details}</p>
-
-            <p className={indexStyles.item}>{description.about}</p>
-
-            <p className={indexStyles.item}>{description.cta}</p>
-
-            <p className={indexStyles.item}>{description.social}</p>
-          </div>
-        </div>
-      </section>{" "}
-      <Footer />;
-    </>
+    <div className={indexStyles.sectionContainer}>
+      <div className={`${indexStyles.body} ${indexStyles.indexContent}`}>
+        <ul className={indexStyles.headerContent}>
+          <li className={indexStyles.headerImg}>
+            <Header home />
+          </li>
+          <li className={indexStyles.intro}>
+            <h1 className={indexStyles.title}>{description.title}</h1>
+            <p>{description.intro}</p>
+            <p>{description.cta}</p>
+            <p>{description.social}</p>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
