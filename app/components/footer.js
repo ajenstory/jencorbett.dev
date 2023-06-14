69d89f12794e55a1d2a64c70dacaf0905cb3723e
jen <crashtestdollynz@gmail.com>
2023-06-14 13:29:53 +1200

    Update footer styles to flex and remove link

--- app/components/footer.js
+++ app/components/footer.js
@@ -2,22 +2,11 @@ import Link from "next/link";
 import footerStyles from "./footer.module.css";
 
 const Footer = () => (
-  <div>
-    <footer className={footerStyles.container}>
-      <div className={footerStyles.wrapper}>
-        <p className={footerStyles.item}>
-          <Link
-            target="_blank"
-            href="https://github.com/ajenstory/jencorbett.dev"
-            title="view this project on github"
-          >
-            View this project on github
-          </Link>
-        </p>
-        <p> © 2023 Jen Corbett</p>
-      </div>
-    </footer>
-  </div>
+  <footer className={footerStyles.footerContainer}>
+    <div className={footerStyles.footerItem}>
+      <p> © 2023 Jen Corbett</p>
+    </div>
+  </footer>
 );
 
 export default Footer;
