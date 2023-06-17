d3661ac1d755876ddf32e66e59def664613fd75f
jen <hello@jencorbett.dev>
2023-06-17 16:19:53 +1200

    remove player

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,11 +1,7 @@
 import footerStyles from "./footer.module.css";
-import Player from "./player.js";
+
 const Footer = () => (
   <footer className={footerStyles.footerContainer}>
-    {" "}
-    <div className={footerStyles.footerPlayer}>
-      <Player />
-    </div>
     <div className={footerStyles.footerItem}>
       <p> © 2023 Jen Corbett</p>
     </div>
